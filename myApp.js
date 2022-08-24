34a5f6ac53e5d43792ca22febd40969db9a85ec6
kam1014 <49304905+kam1014@users.noreply.github.com>
2022-08-23 19:23:25 -0500

    correction

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-app.use(helmet());
+(getUserInput) =>
+  $.get(getUserInput('url') + '/_api/package.json').then(
+    (data) => {
+      const packJson = JSON.parse(data);
+      const helmet = packJson.dependencies.helmet;
+      assert(helmet === '3.21.3' || helmet === '^3.21.3');
+    },
+    (xhr) => {
+      throw new Error(xhr.responseText);
+    }
+  );
 
 
 
